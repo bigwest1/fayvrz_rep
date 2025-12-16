5a9aaab4e284f3f877ec5b4a40a6ef0eeff0ed8a
“bigwest1” <jwestlund@jessewestlund.com>
2025-12-16 16:31:14 -0600

    chore: allow pricing route as public

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/",
   "/example(.*)",
+  "/pricing(.*)",
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/life-plans/example(.*)",
