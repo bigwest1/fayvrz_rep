1214058237af498a78bb813a8d5db9d5620304ea
“bigwest1” <jwestlund@jessewestlund.com>
2025-12-16 15:11:07 -0600

    chore: add formatting and typecheck scripts

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+/** Prettier guardrails for consistent formatting. */
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  printWidth: 88,
+  tabWidth: 2,
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
